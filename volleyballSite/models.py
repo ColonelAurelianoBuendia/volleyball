e997e026ce6ca030bf316adb2c73361fb93657fd
ColonelAurelianoBuendia <hooman.zardosht@gmail.com>
2022-07-24 12:52:08 +0430

    the model completed

--- volleyballSite/models.py
+++ volleyballSite/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+
+class Stadium(models.Model):
+    name = models.CharField(max_length=100)
+    sit_number = models.IntegerField(default=100)
+
+class Match(models.Model):
+    match_time = models.DateTimeField()
+    first_team = models.ForeignKey(Team,on_delete=models.CASCADE,related_name='firstTeam')
+    second_team = models.ForeignKey(Team,on_delete=models.CASCADE,related_name='secondTeam')
+    stadium = models.ForeignKey(Stadium,on_delete=models.CASCADE)
+
+
+
+class Sit(models.Model):
+    number = models.IntegerField(default=0)
+    sitted_user = models.ForeignKey(User, null= True,on_delete=models.SET_NULL)
+    match = models.ForeignKey(Match,on_delete=models.CASCADE)
+
+
