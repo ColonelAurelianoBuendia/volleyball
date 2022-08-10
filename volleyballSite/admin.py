73b75f4a8a6d91000a51cdfb9498ac48c3544867
hooman.zardosht@gmail.com <hooman.zardosht@gmail.com>
2022-08-10 12:41:35 +0430

    login page created with login url

--- volleyballSite/admin.py
+++ volleyballSite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Stadium,Match,Sit,Team
+admin.site.register(Stadium)
+admin.site.register(Match)
+admin.site.register(Sit)
+admin.site.register(Team)
+
--- volleyballSite/views.py
+++ volleyballSite/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     return HttpResponse('hello how are u???')
+
+def login(request):
+    if 'fname' in request.POST:
+
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        return render(request,'login.html',{'fname':fname,'lname':lname})
+
+    return render(request, 'login.html')
