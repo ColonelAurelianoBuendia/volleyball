48a99a60eb34efab6d00ff73cce2714749e327b6
hooman.zardosht@gmail.com <hooman.zardosht@gmail.com>
2022-08-16 11:59:31 +0430

    the home page hase been completed

--- volleyballSite/urls.py
+++ volleyballSite/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/',views.login,name='login')
+    path('login/' , views.login , name='login'),
+    path('register/', views.register ,name='register'),
+
+    path('<int:match_id>/buy',views.buy,name='buy'),
 ]
--- volleyballSite/views.py
+++ volleyballSite/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import User,Match
 def index(request):
     return HttpResponse('hello how are u???')
 
 def login(request):
-    if 'fname' in request.POST:
+    if 'username' in request.POST:
+        try:
+            user = User.objects.get(username=request.POST['username'])
+            return render(request, 'home.html',{'matches' : Match.objects.all()})
 
-        fname = request.POST['fname']
-        lname = request.POST['lname']
-        return render(request,'login.html',{'fname':fname,'lname':lname})
+        except:
+            return render(request, 'login.html', {'error_message': 'username or password are incorrect'})
+    else:
+        return render(request,'login.html')
+def register(request):
+    if 'username' in request.POST:
+        try:
+            user = User.objects.get(username=request.POST['username'])
+            return render(request,'register.html',{'error_message': 'another username registered with given username'})
 
-    return render(request, 'login.html')
+        except:
+            username = request.POST['username']
+            password = request.POST['password']
+            name = request.POST['name']
+
+            user = User(username = username,name = name ,password = password)
+            user.save()
+            return render(request,'login.html',{'error_message' : 'you have been registered successfully'})
+
+    return render(request,'register.html')
+
+
+def buy(request):
+    pass
